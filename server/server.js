28e333450a13b4b578ae28cf9dd5ac9a86b0d319
MeerKar <meku.jithu@gmail.com>
2025-04-02 18:04:05 -0700

    chore: Configure for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const db = require("./config/connection");
 
 const apiRoutes = require("./api"); // Import the API routes
 const { authMiddleware } = require("./utils/auth");
-const { ApolloServer } = require("@apollo/server");
+const { ApolloServer } = require("apollo-server-express");
 const { expressMiddleware } = require("@apollo/server/express4");
 const { typeDefs, resolvers } = require("./schemas");
 
@@ -20,19 +20,14 @@ const server = new ApolloServer({
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
+  server.applyMiddleware({ app });
 
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
-  app.use(
-    "/graphql",
-    expressMiddleware(server, {
-      context: authMiddleware,
-    })
-  );
-
   app.use("/api", apiRoutes); // Use the API routes
 
+  // Serve static files from the React app build directory
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
 
@@ -44,7 +39,9 @@ const startApolloServer = async () => {
   db.once("open", () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
     });
   });
 };
